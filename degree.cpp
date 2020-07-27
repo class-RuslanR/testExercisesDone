0245baf425eaab08a89ac7da7dc5bf8bea7c3f8d
Ruslan Rogozhkin <57987419+class-RuslanR@users.noreply.github.com>
2020-07-27 09:53:53 +0300

    Add files via upload

---
+++ degree.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int Degree(int num)
+{
+    int count = 0;
+    {
+        while (num)
+        {
+            if (num != 1)
+            {
+                ++count;
+            }
+            num >>= 1;
+        }
+        return count;
+    }
+}
+
+int main()
+{
+    int num = 20;
+    std::cout << Degree(num) << std::endl;
+}
